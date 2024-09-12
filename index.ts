d899c97e0dff234232f1754e9073fb15254937fb
Reid Moffat <18rem8@queensu.ca>
2024-09-12 13:20:25 -0400

    proper built-in imports

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import Listr from 'listr';
