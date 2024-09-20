d5d7a1f68cf8243107107cfc9226977878d0967b
Reid Moffat <18rem8@queensu.ca>
2024-09-20 12:23:01 -0400

    moved script to index

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
-import run from './src/script';
+import { promptForPackageDirectory, promptForPackageName, promptForUsingDefaults } from "./src/prompts";
+import Generators from "./src/generators";
+import chalk from "chalk";
+
+const run = async () => {
+    try {
+        const packageName = await promptForPackageName();
+
+        const packageDirectory = await promptForPackageDirectory(packageName);
+
+        await promptForUsingDefaults();
+
+        new Generators(packageName, packageDirectory).runTasks();
+
+        console.log(chalk.green('\nBoilerplate generation completed!'));
+        console.log(chalk.cyan(`Now you can start coding in your new package: ${packageName}`));
+    } catch (error) {
+        console.error(chalk.red('Error occurred:'), error);
+    }
+};
 
 run();
