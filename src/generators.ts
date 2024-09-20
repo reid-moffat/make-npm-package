ad801e3c8a3061478b66ca3ccdbcbecb885e9271
Reid Moffat <18rem8@queensu.ca>
2024-09-20 13:41:59 -0400

    genrate lisence

--- src/generators.ts
+++ src/generators.ts
@@ -25,6 +25,7 @@ class Generators {
         const packageSpinner = yoctoSpinner({ text: 'Creating development files...' }).start();
         this.createPackageJson();
         this.createReadme();
+        this.createLicense();
         this.initGitRepo();
         this.initWorkflows();
         packageSpinner.success('Development files created');
@@ -63,6 +64,14 @@ class Generators {
         addLine('...');
 
         fs.writeFileSync(this._packageDirectory + "/README.md", readmeStr);
+    }
+
+    private createLicense = () => {
+        const license = "MIT License\n\n" +
+            "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n" +
+            "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n" +
+            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+        fs.writeFileSync(this._packageDirectory + "/LICENSE", license);
     }
 
     private initGitRepo = () => {
